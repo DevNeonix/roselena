f1b9b3925076f037afe3c58b7c2da186b4328765
Jolver Gil Quevedo <JQUEVEDO@elmodelador.local>
2019-04-09 13:34:30 -0500

    h22

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,67 @@
+@extends('app')
+@section('content')
+<nav class="navbar">
+  <div class="container">
+    <a href="" class="navbar-brand col-sm-12 col-md-6">Roselena Quevedo Poesía</a>
+  <ul class="nav justify-content-end">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Poemas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Contacto</a>
+    </li>
+  </ul>
+  </div>
+</nav>
+<div class="banner">
+  <div class="ifull"></div>
+    <div class="contenedor text-right">
+      <h1>Poema</h1>
+    <p>Cuantas veces te ame 
+      <br>
+sin restricciones , <br>
+con el encuentro soñado<br>
+de mis emociones.<br>
+Me puse bonita , aquella tarde<br>
+con el vestido blanco<br>
+y el deseo bordado.<br>
+Tantas tardes traviesas<br>
+como noches en vela <br>
+tocándonos en el marco<br>
+de aquella puerta . Hoy si vieras<br>
+como me duele tu ausencia<br>
+pasan los dias<br>
+y la felicidad ya no puedo tenerla.</p>
+  </div>
+</div>
+<div class="main">
+  <div class="container">
+    <div class="row">
+    <div class="col col-md-4 p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae autem distinctio necessitatibus dolor voluptate enim sequi, quod temporibus. Harum voluptatem ut eaque explicabo asperiores libero veniam officia iusto perspiciatis non.</div>
+    <div class="col col-md-4 p-2">Quaerat veniam magnam fugiat et eos vel ducimus ipsam, reprehenderit, repellat recusandae delectus minima obcaecati, modi doloribus quidem laboriosam voluptates ut! Repellendus quis, unde excepturi corrupti officia fuga at architecto.</div>
+    <div class="col col-md-4 p-2">Id excepturi dolor quibusdam eum maiores sed nostrum eveniet possimus magni sit provident laborum, nulla quas et, neque architecto aspernatur consequatur, facere in modi nisi quo? Necessitatibus fuga obcaecati vel!</div>
+  </div>
+  </div>
+</div>
+<footer class="footer">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-6">
+        <h2>Redes</h2>
+        <ul style="list-style:none;text-align:left;">
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>WhatsApp</li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h2>Contacto</h2>
+        <p>942415416 <br> </p>
+      </div>
+    </div>
+  </div>
+  <div class="barrainferior">
+    Todos los derechos reservados Rosa Quevedo 2018
+  </div>
+</footer>
+@endsection
